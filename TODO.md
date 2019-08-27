939248b0c5f5cb5cbab07332dc0d6e2444ce5b69
marcusfg@gmail.com <marcusfg@gmail.com>
2019-08-27 00:52:34 -0400

    Fix molecule instances & make become default

--- TODO.md
+++ TODO.md
@@ -4,12 +4,12 @@ To-Do's
 In-Progress
 -----------
 
-- [ ] Figure out why there's permission errors in /tmp during testing
+- [ ] cleanup output by delegating the install to a loop based on which os_family in use
+- [ ] add tests to verify that the commands themselves work (command -v cmd)
 
 Planning
 --------
 
-- [ ] make default task use `become` & edit dockerfile template to add sudo for testing
 - [ ] consider using `flatten` instead to speed up play execution
 - [ ] fill in README
 - [ ] fill in meta/main
@@ -21,6 +21,8 @@ Future
 Completed
 ---------
 
+- [x] make default task use `become` & edit dockerfile template to add sudo for testing
+- [x] Figure out why there's permission errors in /tmp during testing
 - [x] defaults
 - [x] main task
 - [x] test setup
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,44 +1,56 @@
+""" Tests the role """
 import os
 import pytest
 
 import testinfra.utils.ansible_runner
 
+# pylint: disable=invalid-name
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 @pytest.fixture
 def has_apt(host):
-   return host.run('command -v apt').rc == 0
+    """ Fixture to check for aptitude """
+    return host.run('command -v apt').rc == 0
 
 
 def test_hosts_file(host):
+    """ Verify the hosts file (therefore instance) works """
     f = host.file('/etc/hosts')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
 
-@pytest.mark.parametrize('pkg',[
-    'openssh',
-    'openssl',
-    'wget',
-    'curl',
-    'git',
-    'tig',
-    'vim',
-    'tmux',
-    'fzf',
-    'fd',
-    'ripgrep',
-    'tree',
-    'ranger',
-    'ncdu',
-    'htop',
-    'iotop',
-    'tldr',
+
+@pytest.mark.parametrize('pkg', [
+    {'pacman': 'openssh', 'apt': 'openssh-server'},
+    {'pacman': 'openssl', 'apt': 'openssl'},
+    {'pacman': 'keychain', 'apt': 'keychain'},
+    {'pacman': 'wget', 'apt': 'wget'},
+    {'pacman': 'curl', 'apt': 'curl'},
+    {'pacman': 'unzip', 'apt': 'unzip'},
+    {'pacman': 'git', 'apt': 'git'},
+    {'pacman': 'tig', 'apt': 'tig'},
+    {'pacman': 'vim', 'apt': 'vim'},
+    {'pacman': 'tmux', 'apt': 'tmux'},
+    {'pacman': 'fzf', 'apt': 'fzf'},
+    {'pacman': 'fd', 'apt': 'fd-find'},
+    {'pacman': 'ripgrep', 'apt': 'ripgrep'},
+    {'pacman': 'tree', 'apt': 'tree'},
+    {'pacman': 'ranger', 'apt': 'ranger'},
+    {'pacman': 'ncdu', 'apt': 'ncdu'},
+    {'pacman': 'htop', 'apt': 'htop'},
+    {'pacman': 'iotop', 'apt': 'iotop'},
+    {'pacman': 'tldr', 'apt': 'tldr'},
 ])
 def test_that_common_packages_installed(host, pkg, has_apt):
+    """ Test whether role installed packages are present by package mgr."""
     # in debian-based systems fd is called fd-find
-    if has_apt and pkg == 'fd':
-        pkg = 'fd-find'
-    assert host.package(pkg).is_installed
+    pkg_mgr = 'pacman'
+    if has_apt:
+        pkg_mgr = 'apt'
+    #  if has_apt and pkg == 'fd':
+    #      pkg = 'fd-find'
+    assert host.package(pkg[pkg_mgr]).is_installed
